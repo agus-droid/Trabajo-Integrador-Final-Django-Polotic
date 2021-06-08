d14fe565700ec03b0c0dcbe95e9a0517d1da7073
Agus <agustin9845@gmail.com>
2021-06-08 18:19:19 -0300

    Agregué los requisitos del proyecto y moví una función al main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,8 @@
-console.log('Hola Mundo, mainjs')
+function showPassword() {
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+    }
