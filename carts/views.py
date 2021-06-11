889ed2aa37a80304cc67ea2c8f9198a911e9050b
Agus <agustin9845@gmail.com>
2021-06-10 23:29:12 -0300

    Cart products quantity fix

--- carts/views.py
+++ carts/views.py
@@ -1,7 +1,8 @@
+import products
 from django.shortcuts import get_object_or_404, render, redirect
 from .utils import get_or_create_cart
 from products.models import Product
-
+from .models import CartProducts
 # Create your views here.
 
 def cart(request):   
@@ -13,7 +14,10 @@ def cart(request):
 def add(request):
     cart = get_or_create_cart(request)
     product = get_object_or_404(Product, pk=request.POST.get('product_id'))
-    cart.products.add(product)
+    quantity = int(request.POST.get('quantity', 1))
+    cart_product = CartProducts.objects.create_or_update_quantity(cart=cart,
+                                                                    product=product, 
+                                                                    quantity=quantity)
     return render(request, 'carts/add.html',{
         'product': product
     })
