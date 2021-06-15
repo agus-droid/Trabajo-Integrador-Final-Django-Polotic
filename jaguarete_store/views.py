2183d8df68677607d6b00e345893836572ebe3f6
Agus <agustin9845@gmail.com>
2021-06-15 14:24:36 -0300

    Remove unecessary lines

--- jaguarete_store/views.py
+++ jaguarete_store/views.py
@@ -1,10 +1,8 @@
 from categories.models import Category
 from django.http.response import HttpResponseRedirect
 from products.models import Product
-#from jaguarete_store.forms import RegisterForm
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth import authenticate, login, logout
-#from django.contrib.auth.models import User
 from users.models import User
 from django.contrib import messages
 from .forms import RegisterForm
